e709e32c747a3771d7b56fbb1c8f18fed7ad0880
Cha Krupka <chakrupka238@gmail.com>
2024-06-08 02:13:07 -0400

    settings page created

---
+++ src/screens/Settings/Settings.jsx
@@ -0,0 +1,53 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../Login/Login.css";
+
+const Settings = () => {
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    window.localStorage.clear();
+    navigate("/");
+  };
+
+  return (
+    <div style={{ width: "99dvw", height: "95dvh", marginTop: "4dvh" }}>
+      <Link to={"/"} className="backButton">
+        Home
+      </Link>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginLeft: "8%",
+          width: "84%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "-10dvh",
+          fontSize: "4dvh",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ width: "50dvh" }}>
+          <div>🚧 Under Construction 🚧</div>
+          <div style={{ fontSize: "3dvh", marginBottom: "3dvh" }}>
+            More options coming soon. If you forgot your password, contact Cha.
+          </div>
+          <div
+            onClick={handleLogOut}
+            className="loginButton"
+            style={{
+              marginLeft: "13.5dvh",
+              width: "22dvh",
+              paddingTop: "1dvh",
+            }}
+          >
+            👋 Log Out
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;

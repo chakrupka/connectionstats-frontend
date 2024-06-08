f6b130b48fcf15ee4b44c79293a4548a42447227
Cha Krupka <chakrupka238@gmail.com>
2024-06-08 02:12:28 -0400

    updated to accomodate user token

--- src/screens/SubmitGame/SubmitGame.jsx
+++ src/screens/SubmitGame/SubmitGame.jsx
@@ -1,25 +1,21 @@
 import { prepGame } from "../../utils/formatGame.js";
 import { sendGame, setToken } from "../../services/game_service.js";
-import { login } from "../../services/login_service.js";
 import "./SubmitGame.css";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SubmitGame = () => {
   const [newGame, setNewGame] = useState("");
-  const [newName, setNewName] = useState("");
   const [results, setResults] = useState(null);
-  const [next, setNext] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
-  const [errMessage, setErrMessage] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedUser");
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
-      setUser(user);
+      setToken(user.token);
+    } else {
+      navigate("/");
     }
   }, []);
 
@@ -30,7 +26,8 @@ const SubmitGame = () => {
   4. "Share Your Results"
   5. If on mobile, find the "Copy" option
   6. Return & "Paste Game"
-  7. Accept prompts`;
+  7. Accept prompts
+  8. Submit`;
 
   const handleGameChange = (event) => {
     setNewGame(event.target.value);
@@ -40,12 +37,6 @@ const SubmitGame = () => {
   const handleNameChange = (event) => {
     setNewName(event.target.value);
     console.log(newGame);
-  };
-
-  const goNext = (event) => {
-    event.preventDefault();
-    // add test
-    setNext(true);
   };
 
   const submitGame = (event) => {
@@ -71,125 +62,44 @@ const SubmitGame = () => {
     });
   };
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    try {
-      const user = await login({ username, password });
-      window.localStorage.setItem("loggedUser", JSON.stringify(user));
-
-      setToken(user.token);
-      setUser(user);
-      setUsername("");
-      setPassword("");
-    } catch (err) {
-      setErrMessage("Incorrect credentials");
-      setTimeout(() => {
-        setErrMessage(null);
-      }, 3000);
-    }
-  };
-
-  const submitForm = () => (
-    <div className="inputSection">
-      {!results ? (
-        !next ? (
-          <form onSubmit={goNext} className="gameForm">
-            <button
-              type="button"
-              className="submitButton"
-              onClick={() => pasteClipboardContent()}
-            >
-              Paste Game
-            </button>
-            <textarea
-              value={newGame}
-              onChange={handleGameChange}
-              className="gameInput"
-              id="paste-target"
-              placeholder={placeholder}
-            />
-            <button type="submit" className="submitButton">
-              Next
-            </button>
-          </form>
-        ) : (
-          <div className="enterNameContainer">
-            <div style={{ fontSize: 37, marginTop: "10dvh" }}>
-              Enter a name:
-            </div>
-            <form onSubmit={submitGame} className="nameForm">
-              <input
-                className="nameInput"
-                type="text"
-                value={newName}
-                onChange={handleNameChange}
-              ></input>
-              <button type="submit" className="submitButton">
-                Submit
-              </button>
-            </form>
-          </div>
-        )
-      ) : (
-        <div className="results">
-          <div>Completed: {results.score ? "Yes" : "No"}</div>
-          <div>Number of guesses: {results.tries}</div>
-          <div>Score: {results.score ? results.score : "0"}</div>
-          <Link to={"/leaderboard"} className="checkLeaderboard">
-            Check Leaderboard
-          </Link>
-        </div>
-      )}
-    </div>
-  );
-
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          autocapitalize="off"
-          required
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="text"
-          required
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
-
   return (
     <>
-      {errMessage !== null && (
-        <div style={{ width: "80dvw", height: "3dvh", background: "lightred" }}>
-          {errMessage}
-        </div>
-      )}
       <div style={{ marginTop: "4dvh" }}>
         <Link to={"/"} className="backButton">
           Home
         </Link>
-        {user === null ? (
-          loginForm()
-        ) : (
-          <div>
-            <p>Hello, {user.name}</p>
-            {submitForm()}
-          </div>
-        )}
+        <div className="inputSection">
+          {!results ? (
+            <form onSubmit={submitGame} className="gameForm">
+              <button
+                type="button"
+                className="submitButton"
+                onClick={() => pasteClipboardContent()}
+              >
+                Paste Game
+              </button>
+              <textarea
+                value={newGame}
+                onChange={handleGameChange}
+                className="gameInput"
+                id="paste-target"
+                placeholder={placeholder}
+              />
+              <button type="submit" className="submitButton">
+                Submit
+              </button>
+            </form>
+          ) : (
+            <div className="results">
+              <div>Completed: {results.score ? "Yes" : "No"}</div>
+              <div>Number of guesses: {results.tries}</div>
+              <div>Score: {results.score ? results.score : "0"}</div>
+              <Link to={"/leaderboard"} className="checkLeaderboard">
+                Check Leaderboard
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );

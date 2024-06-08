ff8ef166e597f116447573f5b67d33acaddbebcf
Cha Krupka <chakrupka238@gmail.com>
2024-06-08 02:40:21 -0400

    load fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import GuestHome from "./screens/Home/GuestHome";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedUser");
@@ -11,7 +12,12 @@ const App = () => {
       const user = JSON.parse(loggedUserJSON);
       setUser(user);
     }
+    setIsLoading(false);
   }, []);
+
+  if (isLoading) {
+    return null;
+  }
 
   return user === null ? <GuestHome /> : <UserHome user={user} />;
 };

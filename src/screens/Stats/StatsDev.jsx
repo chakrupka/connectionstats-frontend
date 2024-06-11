0c88284028fae1f5a319148511321f4c28269ae8
Cha Krupka <chakrupka238@gmail.com>
2024-06-11 14:59:50 -0600

    styling

---
+++ src/screens/Stats/StatsDev.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getUserGames, setToken } from "../../services/game_service";
+import { Link } from "react-router-dom";
+import stats from "../../utils/stats";
+import sampleGames from "../../utils/games_for_testing";
+import "./Stats.css";
+
+const StatsDev = ({ userProp }) => {
+  const games = sampleGames;
+
+  const DayView = ({ day, game }) => {
+    const dayTag = day.slice(0, 3);
+    return (
+      <div className="dayBox">
+        <div className="dayTag">{dayTag}</div>
+      </div>
+    );
+  };
+
+  const SolveByDay = (games) => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    return (
+      <div className="solveByDayMain">
+        {days.map((day, index) => (
+          <DayView day={day} key={index} />
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className="statsMain">
+      <div>Hello</div>
+      <SolveByDay />
+    </div>
+  );
+};
+
+export default StatsDev;

62824d318a138695a7256dd9b29265bb874d3efe
Cha Krupka <chakrupka238@gmail.com>
2024-06-15 11:55:24 -0600

    added stats to store

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import gamesReducer from "../reducers/games_reducer";
 import userReducer from "../reducers/user_reducer";
+import statsReducer from "../reducers/stats_reducer";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     games: gamesReducer,
+    stats: statsReducer,
   },
 });
 

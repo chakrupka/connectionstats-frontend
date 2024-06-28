ed9a6b3cb708d8a9ae0f6648533b55c6076d4c52
Cha Krupka <chakrupka238@gmail.com>
2024-06-27 19:56:59 -0700

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { Provider, useDispatch } from "react-redux";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.jsx";
 import store from "./stores/store.js";
